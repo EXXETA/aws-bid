d9668bd9cbaabea3296cbab3da9dc7bc1c044576
Nils B <nils.botthaeuser@exxeta.com>
2022-01-20 22:59:10 +0100

    Use relative production config path for web

--- website/src/environments/environment.prod.ts
+++ website/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  configBaseUrl: 'http://exxeta-aws-bid-exxetacloudbidexxetaawsbid53573186-vrt5hzj33jea.s3-website.eu-central-1.amazonaws.com/'
+  configBaseUrl: './'
 };
